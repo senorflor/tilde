227bca6ca9580d6cf9611a1c215147c8ce3b2959
Patrick Flor <git.push.origin@gmail.com>
2017-05-09 07:32:53 -0500

    Flush out super old profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,2 @@
-;;; TODO: this is really old; update it.
-{:user {:plugins [ ;;[lein-nodisassemble "0.1.2"]
-                  [jonase/eastwood "0.1.4"]
-                  [lein-try "0.4.3"]
-                  [lein-gorilla "0.3.3"]
-                  [lein-exec "0.3.4"]
-                  [lein-pprint "1.1.2"]]
-        :dependencies [[slamhound "1.5.5"]
-                       [criterium "0.4.3"]
-                       [org.clojure/tools.nrepl "0.2.10"]]
-        :repl-options {:init (require '[criterium.core :as crit])}}}
+{:user {:plugins [[lein-plz "0.4.0-SNAPSHOT"
+                   ]]}}
