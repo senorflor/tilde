97a79da4d3a1928a68c4f64f44a86f642cf9a76f
Patrick Flor <git.push.origin@gmail.com>
2018-04-07 06:24:36 -0500

    Add typescript-in-emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -47,6 +47,8 @@
                       flow-minor-mode
                       web-mode
                       prettier-js
+                      tide ; Typescript
+                      company ; optional tide dep
 
                    ;; Elm
                       elm-mode)
@@ -197,6 +199,27 @@
 (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
 (add-hook 'web-mode-hook 'customize-web-mode-formatting)
 (add-hook 'web-mode-hook 'flow-minor-enable-automatically)
+
+;;; Tide
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx?\\'" . web-mode))
 
 ;;; org mode!
 (require 'org-install)
